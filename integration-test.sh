40c632db1f155a292eac47e2c3dd6935514e3cfd
Shai Badarna <shai.badarna@avanade.com>
2023-11-20 23:15:32 +0100

    script updated4

--- integration-test.sh
+++ integration-test.sh
@@ -13,16 +13,16 @@ echo "Port: $PORT"
 FULL_URL="http://devsecops-demodns.eastus.cloudapp.azure.com:$PORT/increment/99"
 echo "Full URL: $FULL_URL"
 
-# Envoie une requête HTTP à l'application avec sortie détaillée pour le débogage et récupère la réponse.
-response=$(curl -s -v $FULL_URL 2>&1)
-http_code=$(curl -s -o /dev/null -w "%{http_code}" -v $FULL_URL 2>&1)
+# Envoie une requête HTTP à l'application avec sortie détaillée pour le débogage et récupère la réponse complète.
+full_response=$(curl -s -v $FULL_URL 2>&1)
+echo "Full Response: $full_response"
 
-# Affiche la réponse et le code de statut HTTP pour le débogage.
-echo "Response: $response"
+# Envoie une autre requête HTTP à l'application pour récupérer uniquement le code de statut HTTP.
+http_code=$(curl -s -o /dev/null -w "%{http_code}" $FULL_URL)
 echo "HTTP Code: $http_code"
 
 # Vérifie si la réponse de l'application est égale à 100.
-if [[ "$response" == *"100"* ]]; then
+if [[ "$full_response" == *"100"* ]]; then
     echo "Increment Test Passed"
 else
     echo "Increment Test Failed"
@@ -30,7 +30,7 @@ else
 fi
 
 # Vérifie si le code de statut HTTP est 200.
-if [[ "$http_code" == 200 ]]; then
+if [[ "$http_code" == "200" ]]; then
     echo "HTTP Status Code Test Passed"
 else
     echo "HTTP Status code is not 200"
