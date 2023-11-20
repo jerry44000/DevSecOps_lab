d9ebaa3cae1bc748b195985b7a7b957c9aaa3503
Shai Badarna <shai.badarna@avanade.com>
2023-11-20 22:07:58 +0100

    integration test added

---
+++ integration-test.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#integration-test.sh
+
+# Pause l'exécution du script pendant 5 secondes.
+sleep 5s
+
+# Récupère le port NodePort du service Kubernetes spécifié et l'assigne à la variable PORT.
+PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
+
+# Affiche la valeur du port récupéré.
+echo $PORT
+
+# Construit et affiche l'URL complète pour accéder à l'application. Variables d'env dans le Jenkinsfile.
+echo $applicationURL:$PORT/$applicationURI
+
+# Vérifie si la variable PORT n'est pas vide.
+if [[ ! -z "$PORT" ]];
+then
+
+    # Envoie une requête HTTP à l'application et stocke la réponse dans la variable 'response'.
+    response=$(curl -s $applicationURL:$PORT$applicationURI)
+
+    # Envoie une requête HTTP à l'application et récupère seulement le code de statut HTTP.
+    http_code=$(curl -s -o /dev/null -w "%{http_code}" $applicationURL:$PORT$applicationURI)
+
+    # Vérifie si la réponse de l'application est égale à 100.
+    if [[ "$response" == 100 ]];
+        then
+            echo "Increment Test Passed" # Test réussi si la réponse est 100.
+        else
+            echo "Increment Test Failed" # Test échoué si la réponse n'est pas 100.
+            exit 1; # Arrête l'exécution du script avec un code de sortie 1.
+    fi;
+
+    # Vérifie si le code de statut HTTP est 200.
+    if [[ "$http_code" == 200 ]];
+        then
+            echo "HTTP Status Code Test Passed" # Test réussi si le code est 200.
+        else
+            echo "HTTP Status code is not 200" # Test échoué si le code n'est pas 200.
+            exit 1; # Arrête l'exécution du script avec un code de sortie 1.
+    fi;
+
+else
+    echo "The Service does not have a NodePort" # Aucun NodePort trouvé pour le service.
+    exit 1; # Arrête l'exécution du script avec un code de sortie 1.
+fi;
