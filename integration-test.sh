9782852e846e9bbd3fc0ad8a7b379986a3f6e023
Shai Badarna <shai.badarna@avanade.com>
2023-11-20 23:07:34 +0100

    script updated3

--- integration-test.sh
+++ integration-test.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
-# Pause l'exécution du script pendant 5 secondes.
+#integration-test.sh
+
 sleep 5s
 
-# Récupère le port NodePort du service Kubernetes spécifié.
-PORT=$(kubectl -n default get svc ${serviceName} -o jsonpath='{.spec.ports[0].nodePort}')
+PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
 
-# Affiche la valeur du port récupéré.
-echo "Port: $PORT"
+echo $PORT
+echo $applicationURL:$PORT/$applicationURI
 
-# Corrige l'URL pour supprimer la double barre oblique.
-FULL_URL="http://devsecops-demodns.eastus.cloudapp.azure.com:$PORT/increment/99"
-echo "Full URL: $FULL_URL"
+if [[ ! -z "$PORT" ]];
+then
 
-# Envoie une requête HTTP à l'application et récupère la réponse et le code de statut HTTP.
-response=$(curl -s -v $FULL_URL)
-http_code=$(curl -s -o /dev/null -w "%{http_code}" -v $FULL_URL)
+    response=$(curl -s $applicationURL:$PORT$applicationURI)
+    http_code=$(curl -s -o /dev/null -w "%{http_code}" $applicationURL:$PORT$applicationURI)
 
-# Affiche la réponse et le code de statut HTTP pour le débogage.
-echo "Response: $response"
-echo "HTTP Code: $http_code"
+    if [[ "$response" == 100 ]];
+        then
+            echo "Increment Test Passed"
+        else
+            echo "Increment Test Failed"
+            exit 1;
+    fi;
 
-# Vérifie si la réponse de l'application est égale à 100.
-if [[ "$response" == 100 ]]; then
-    echo "Increment Test Passed"
+    if [[ "$http_code" == 200 ]];
+        then
+            echo "HTTP Status Code Test Passed"
+        else
+            echo "HTTP Status code is not 200"
+            exit 1;
+    fi;
+
 else
-    echo "Increment Test Failed"
-    exit 1;
-fi
-
-# Vérifie si le code de statut HTTP est 200.
-if [[ "$http_code" == 200 ]]; then
-    echo "HTTP Status Code Test Passed"
-else
-    echo "HTTP Status code is not 200"
-    exit 1;
-fi
+        echo "The Service does not have a NodePort"
+        exit 1;
+fi;
