dfa075074bca9ba50753df4b1f8985924f0b29f8
Shai Badarna <shai.badarna@avanade.com>
2023-11-21 22:04:28 +0100

    integration test updated

--- Jenkinsfile
+++ Jenkinsfile
@@ -120,6 +120,14 @@ pipeline {
       }
     }
 
+    // stage('OWASP ZAP: DAST') {
+    //   steps {
+    //     withKubeConfig([credentialsId: 'kubeconfig']) {
+    //       sh 'bash zap.sh'
+    //     }
+    //   }
+    // }
+
   }
 
   post {
--- integration-test.sh
+++ integration-test.sh
@@ -1,38 +1,46 @@
 #!/bin/bash
 
-# Pause l'exécution du script pendant 5 secondes.
-sleep 5s
+# Augmenter le temps d'attente pour permettre à l'application de démarrer complètement.
+echo "Attente de démarrage de l'application..."
+sleep 15s
 
 # Récupère le port NodePort du service Kubernetes spécifié.
 PORT=$(kubectl -n default get svc $serviceName -o jsonpath='{.spec.ports[0].nodePort}')
-
-# Affiche la valeur du port récupéré.
-echo "Port: $PORT"
+echo "Port récupéré: $PORT"
 
 # Corrige l'URL pour supprimer toute double barre oblique.
 FULL_URL="http://devsecops-demodns.eastus.cloudapp.azure.com:$PORT/increment/99"
-echo "Full URL: $FULL_URL"
+echo "URL complète: $FULL_URL"
 
-# Envoie une requête HTTP à l'application avec sortie détaillée pour le débogage et récupère la réponse complète.
-full_response=$(curl -s -v $FULL_URL 2>&1)
-echo "Full Response: $full_response"
+# Boucle de tentative de connexion avec réessais en cas d'échec.
+MAX_ATTEMPTS=5
+attempt=1
+while [ $attempt -le $MAX_ATTEMPTS ]; do
+    echo "Tentative $attempt de connexion..."
 
-# Envoie une autre requête HTTP à l'application pour récupérer uniquement le code de statut HTTP.
-http_code=$(curl -s -o /dev/null -w "%{http_code}" $FULL_URL)
-echo "HTTP Code: $http_code"
+    # Envoie une requête HTTP à l'application et récupère la réponse complète.
+    full_response=$(curl -s -v $FULL_URL 2>&1)
+    echo "Réponse complète: $full_response"
 
-# Vérifie si la réponse de l'application est égale à 100.
-if [[ "$full_response" == *"100"* ]]; then
-    echo "Increment Test Passed"
-else
-    echo "Increment Test Failed"
-    exit 1;
-fi
+    # Récupère le code de statut HTTP.
+    http_code=$(curl -s -o /dev/null -w "%{http_code}" $FULL_URL)
+    echo "Code HTTP: $http_code"
 
-# Vérifie si le code de statut HTTP est 200.
-if [[ "$http_code" == "200" ]]; then
-    echo "HTTP Status Code Test Passed"
-else
-    echo "HTTP Status code is not 200"
-    exit 1;
-fi
+    if [[ "$http_code" == "200" ]]; then
+        echo "Connexion réussie. Test d'incrément en cours..."
+        if [[ "$full_response" == *"100"* ]]; then
+            echo "Test d'incrément réussi."
+            exit 0
+        else
+            echo "Test d'incrément échoué."
+            exit 1
+        fi
+    else
+        echo "Échec de la connexion. Réessai dans 5 secondes..."
+        sleep 5s
+    fi
+    ((attempt++))
+done
+
+echo "Échec après $MAX_ATTEMPTS tentatives."
+exit 1
